c37c06ae275fecc20cebea0d1083596bfdf6f022
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2020-12-17 05:31:42 +0300

    redux

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import counter from '../../hocs/counter'
+// import counter from '../../hocs/counter'
 import Plus from '../plus'
 import Minus from '../minus'
 import styles from './product.module.css'
+import {decrement, increment} from '../../redux/actionCreators/actions'
 
 function Product(props) {
 	const {product, amount, decrement, increment} = props
+	const {id, name, ingredients, price} = product
 
 	return (
 		<div className={styles.product} data-test="product">
 			<div className={styles.content}>
 				<div>
-					<h4 className={styles.title}>{product.name}</h4>
-					<p className={styles.description}>{product.ingredients.join(', ')}</p>
-					<div className={styles.price}>{product.price} $</div>
+					<h4 className={styles.title}>{name}</h4>
+					<p className={styles.description}>{ingredients.join(', ')}</p>
+					<div className={styles.price}>{price} $</div>
 				</div>
 				<div>
 					<div className={styles.counter}>
@@ -24,14 +27,14 @@ function Product(props) {
 						<div className={styles.buttons}>
 							<button
 								className={styles.button}
-								onClick={decrement}
+								onClick={() => decrement(id)}
 								data-test="product-decrement"
 							>
 								<Minus />
 							</button>
 							<button
 								className={styles.button}
-								onClick={increment}
+								onClick={() => increment(id)}
 								data-test="product-increment"
 							>
 								<Plus />
@@ -50,9 +53,24 @@ Product.propTypes = {
 		ingredients: PropTypes.array.isRequired,
 		price: PropTypes.number.isRequired,
 	}).isRequired,
-	amount: PropTypes.number.isRequired,
+	amount: PropTypes.number,
 	decrement: PropTypes.func.isRequired,
 	increment: PropTypes.func.isRequired,
 }
 
-export default counter(Product)
+const mapStateToProps = (state, ownProps) => ({
+	amount: state.order[ownProps.product.id] || 0,
+})
+
+const mapDispatchToProps = {
+	decrement,
+	increment,
+}
+
+// const mapDispatchToProps = (dispatch, ownProps) => ({
+// 	decrement: () => dispatch(decrement(ownProps.product.id)),
+// 	increment: () => dispatch(increment(ownProps.product.id))
+// })
+// и тогда можно сделать <button onClick={decrement}>
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product)
--- src/redux/reducers/orderReducer.js
+++ src/redux/reducers/orderReducer.js
@@ -1,11 +1,16 @@
-const orderReducer = (amount = 0, action) => {
-	switch (action.type) {
-		case 'decrement':
-			return amount - 1
-		case 'increment':
-			return amount + 1
+import {DECREMENT, INCREMENT} from '../constants'
+
+// { [productId]: amount }
+const orderReducer = (state = {}, action) => {
+	const {type, payload} = action
+
+	switch (type) {
+		case DECREMENT:
+			return {...state, [payload.id]: (state[payload.id] || 0) - 1}
+		case INCREMENT:
+			return {...state, [payload.id]: (state[payload.id] || 0) + 1}
 		default:
-			return amount
+			return state
 	}
 }
 
