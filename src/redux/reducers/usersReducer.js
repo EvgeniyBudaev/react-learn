9578a367d6a9593f1fb26610a44d75dbcf984158
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2020-12-19 09:22:10 +0300

    add immer

--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,21 +1,28 @@
+import produce from 'immer'
 import { ADD_REVIEW } from '../constants';
 import { normalizedUsers } from '../../fixtures';
 import { arrToMap } from '../utils';
 
-const usersReducer = (state = arrToMap(normalizedUsers), action) => {
+// Используется immer без мутирования данных
+const usersReducer = produce((draft = arrToMap(normalizedUsers), action) => {
 	const { type, payload, userId } = action;
 
 	switch (type) {
 		case ADD_REVIEW:
-			const { name } = payload.review;
-			return {
-				...state,
-				[userId]: { id: userId, name },
-			};
+			const {name} = payload.review
+			draft[userId] =  {id: userId, name}
+			break;
+
+		// case ADD_REVIEW:
+		// 	const { name } = payload.review;
+		// 	return {
+		// 		...state,
+		// 		[userId]: { id: userId, name },
+		// 	};
 
 		default:
-			return state;
+			return draft;
 	}
-};
+});
 
 export default usersReducer;
