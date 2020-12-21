c0874ac66fafbb115e2087fd0e6ab9f7e6a51d0e
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2020-12-21 07:18:20 +0300

    add react-transition-group

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -4,11 +4,14 @@ import { createStructuredSelector } from 'reselect'
 import {Link} from 'react-router-dom'
 
 import styles from './basket.module.css'
+import './basket.css'
 import BasketRow from './basket-row'
 import BasketItem from './basket-item'
 import Button from '../UI/button'
 import {orderProductsSelector, totalSelector} from '../../redux/selectors'
 import userContext, {UserConsumer} from '../../contexts/user-context'
+import TransitionGroup from 'react-transition-group'
+import CSSTransition from 'react-transition-group'
 
 function Basket({title = 'Basket', total, orderProducts}) {
 	// const {name} = useContext(userContext)
@@ -29,15 +32,18 @@ function Basket({title = 'Basket', total, orderProducts}) {
 					{({name}) => `${name}'s ${title}`}
 				</UserConsumer>
 			</h4>
-			{orderProducts.map(({ product, amount, subtotal, restaurantId }) => (
-				<BasketItem
-					product={product}
-					amount={amount}
-					key={product.id}
-					subtotal={subtotal}
-					restaurantId={restaurantId}
-				/>
-			))}
+			<TransitionGroup>
+				{orderProducts.map(({ product, amount, subtotal, restaurantId }) => (
+					<CSSTransition key={product.id} timeout={5000} classNames="basket-animation">
+						<BasketItem
+							product={product}
+							amount={amount}
+							subtotal={subtotal}
+							restaurantId={restaurantId}
+						/>
+					</CSSTransition>
+				))}
+			</TransitionGroup>
 			<hr className={styles.hr} />
 			<BasketRow label="Sub-total" content={`${total} $`} />
 			<BasketRow label="Delivery costs:" content="FREE" />
